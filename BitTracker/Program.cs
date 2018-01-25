6c3dbb42f8775848f5b5962c5ac39e2e7a8aa801
Nathaniel <nathanielps@gmail.com>
2018-01-24 19:59:10 -0600

    0.1

---
+++ BitTracker/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Gtk;
+
+namespace BitTracker
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			CoinStats coinStats = BitStats.GetCoinStats("BTC-USD");
+			BlockHash blockHash = BitStats.GetBlockStats();
+
+			Application.Init();
+			MainWindow win = new MainWindow();
+			win.Title = "BitTracker";
+			win.KeepAbove = true;
+
+			HBox hbox1 = new HBox(false, 5);
+			HBox hbox2 = new HBox(false, 5);
+			VBox vbox = new VBox(false, 5);
+			win.Add(vbox);
+			vbox.PackStart(hbox1, false, false, 10);
+			vbox.PackStart(hbox2, false, false, 10);
+
+			float coinStatsChange = ((coinStats.last / coinStats.open) * 100 - 100);
+			string coinStatsLabelString = "Opening:\t\t" + coinStats.open.ToString("0.00") + 
+			                                                        "\nCurrent:\t\t" + coinStats.last.ToString("0.00") + 
+			                                                        "\nChange\t\t" + coinStatsChange.ToString("0.00") + "%" + (coinStatsChange > 0 ? "\u2191" : "\u2193");
+			Label coinStatsLabel = new Label();
+			coinStatsLabel.Text = coinStatsLabelString;
+			Frame coinStatsFrame = new Frame();
+			coinStatsFrame.Label = "24-hour Stats";
+			coinStatsFrame.Add(coinStatsLabel);
+			hbox1.PackStart(coinStatsFrame, false, false, 0);
+
+			List<string> blockStatsLabelString = new List<string>();
+			string prevblockHash = blockHash.hash;
+			float blockHashTime = blockHash.time / 3600000 - 300;
+			blockStatsLabelString.Add(blockHash.hash + "\t" + blockHash.height.ToString() + "\t" + blockHashTime.ToString("00:00"));
+			Label blockStatsLabel = new Label();
+			blockStatsLabel.Text = "Hash:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tHeight:\tTime:\n" + blockStatsLabelString[0];
+			Frame blockStatsFrame = new Frame();
+			blockStatsFrame.Label = "Latest Blocks";
+			blockStatsFrame.Add(blockStatsLabel);
+			hbox2.PackStart(blockStatsFrame, false, false, 0);
+
+			uint timer = GLib.Timeout.Add(10000, new GLib.TimeoutHandler(() =>
+			{
+				coinStats = BitStats.GetCoinStats("BTC-USD");
+				coinStatsChange = ((coinStats.last / coinStats.open) * 100 - 100);
+				coinStatsLabelString = "Opening:\t\t" + coinStats.open.ToString("0.00") + 
+				                                                 "\nCurrent:\t\t" + coinStats.last.ToString("0.00") + 
+				                                                 "\nChange\t\t" + coinStatsChange.ToString("0.00") + "%" + (coinStatsChange > 0 ? "\u2191" : "\u2193");				coinStatsLabel.Text = coinStatsLabelString;
+
+				blockHash = BitStats.GetBlockStats();
+				if (blockHash.hash != prevblockHash)
+				{
+					blockHashTime = blockHash.time / 3600000 - 300;
+					blockStatsLabelString.Insert(0, blockHash.hash + "\t" + blockHash.height.ToString() + "\t" + blockHashTime.ToString("00:00") + "\n");
+					prevblockHash = blockHash.hash;
+				}
+				string toBlockStatsLabel = "Hash:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tHeight:\tTime:\n";
+				foreach (string s in blockStatsLabelString)
+				{
+					toBlockStatsLabel += s;
+				}
+				blockStatsLabel.Text = toBlockStatsLabel;
+
+				return true;
+			}));
+
+			win.ShowAll();
+			Application.Run();
+		}
+	}
+}
---
+++ BitTracker/Stats.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+public class CoinStats
+{
+	public float open { get; set; }
+	public float high { get; set; }
+	public float low { get; set; }
+	public float volume { get; set; }
+	public float last { get; set; }
+	public float volume_30day { get; set; }
+}
+
+public class BlockHash
+{
+	public string hash { get; set; }
+	public int time { get; set; }
+	public int block_index { get; set; }
+	public int height { get; set; }
+	public int[] txIndexes { get; set; }
+}
+
+class BitStats
+{
+	public static CoinStats GetCoinStats(string product)
+	{
+		string base_url = "https://api.gdax.com/";
+
+		HttpWebRequest stats_request = (HttpWebRequest) WebRequest.Create(base_url + "products/" + product + "/stats");
+		stats_request.Method = WebRequestMethods.Http.Get;
+		stats_request.ContentType = "application/json; charset=utf-8";
+		stats_request.UserAgent = "Test Blockchain Stats Application";
+
+		HttpWebResponse stats_response = stats_request.GetResponse() as HttpWebResponse;
+
+		using (Stream responseStream = stats_response.GetResponseStream())
+		{
+			StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+			CoinStats stats = JsonConvert.DeserializeObject<CoinStats>(reader.ReadToEnd());
+
+			return stats;
+		}
+	}
+
+	public static BlockHash GetBlockStats()
+	{
+		string base_url = "https://blockchain.info/latestblock";
+
+		HttpWebRequest block_request = (HttpWebRequest)WebRequest.Create(base_url);
+		block_request.Method = WebRequestMethods.Http.Get;
+		block_request.ContentType = "application/json; charset=utf-8";
+		block_request.UserAgent = "Test Blockchain Stats Application";
+
+		HttpWebResponse block_response = block_request.GetResponse() as HttpWebResponse;
+
+		using (Stream responseStream = block_response.GetResponseStream())
+		{
+			StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+			BlockHash blockHash = JsonConvert.DeserializeObject<BlockHash>(reader.ReadToEnd());
+
+			return blockHash;
+		}
+	}
+}
